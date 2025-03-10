415d36cc7d4a0d3adce522e62f1aff1f7498c7de
Renato <renats.mc@gmail.com>
2025-03-10 13:49:44 -0300

    finalizando o projeto

--- frontend/javascript_entendendo_promises_e_async_await/scripts.js
+++ frontend/javascript_entendendo_promises_e_async_await/scripts.js
@@ -80,3 +80,48 @@ inputTags.addEventListener("keypress", async (evento) => {
         }
     }
 })
+
+
+const botaoPublicar = document.querySelector(".botao-publicar");
+
+async function publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const deuCerto = Math.random() > 0.5;
+
+            if (deuCerto) {
+                resolve("Pojeto publicado com sucesso.");
+            } else {
+                reject("Erro ao publicar o projeto.");
+            }
+        }, 2000);
+    });
+}
+
+botaoPublicar.addEventListener("click", async (evento) => {
+    evento.preventDefault();
+    const nomeDoProjeto = document.getElementById("nome").value;
+    const descricaoDoProjeto = document.getElementById("descricao").value;
+    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
+
+    try {
+        const resultado = await publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto);
+        console.log(resultado);
+        alert("Deu tudo certo!");        
+    } catch (error) {
+        console.error("Deu errado: ", error);
+        alert("Deu tudo errado!");
+    }
+});
+
+const botaoDescartar = document.querySelector(".botao-descartar");
+
+botaoDescartar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    const formulario = document.querySelector("form");
+    formulario.reset();
+
+    imagemPrincipal.src = "./img/imagem1.png";
+    nomeDaImagem.textContent = "image_projeto.png";
+    listaTags.innerHTML = "";
+})
